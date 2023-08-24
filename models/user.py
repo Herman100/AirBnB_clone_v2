8b896355dae89310628f3114e2b27225be04d2c9
Herman100 <hermankwamebour30@gmail.com>
2023-08-24 20:48:36 +0000

    user update

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base  # import Base
+from sqlalchemy import Column, String  # import necessary SQLAlchemy classes
 
 
-class User(BaseModel):
+class User(BaseModel, Base):  # make User inherit from Base
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    __tablename__ = 'users'  # set table name
+    email = Column(String(128), nullable=False)  # set email column
+    password = Column(String(128), nullable=False)  # set password column
+    first_name = Column(String(128), nullable=True)  # set first_name column
+    last_name = Column(String(128), nullable=True)  # set last_name column
