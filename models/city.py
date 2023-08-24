61560925f42bfd100fafa4719f38f8cb5f6b8b90
Herman100 <hermankwamebour30@gmail.com>
2023-08-24 22:21:52 +0000

    models updated

--- models/city.py
+++ models/city.py
@@ -2,9 +2,13 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    # Establish relationship with Place
+    places = relationship("Place", backref="cities", cascade="all, delete")
--- models/user.py
+++ models/user.py
@@ -2,12 +2,16 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base  # import Base
 from sqlalchemy import Column, String  # import necessary SQLAlchemy classes
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):  # make User inherit from Base
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'  # set table name
-    email = Column(String(128), nullable=False)  # set email column
-    password = Column(String(128), nullable=False)  # set password column
-    first_name = Column(String(128), nullable=True)  # set first_name column
-    last_name = Column(String(128), nullable=True)  # set last_name column
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    # Establish relationship with Place
+    places = relationship("Place", backref="user", cascade="all, delete")
