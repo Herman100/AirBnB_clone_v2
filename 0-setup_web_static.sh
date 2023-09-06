472f66cceb56c027abfa0b06a75edaed5ad74dd2
Herman100 <hermankwamebour30@gmail.com>
2023-09-06 23:24:16 +0000

    made executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 # creates a deployment script for web static
 
-sudo apt-get -y update
-if ! command -v nginx &> /dev/null; then
-    sudo apt-get -y install nginx
-fi
+sudo apt-get update
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+sudo mkdir /data/web_static/shared/
 echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
 
 if [ -L "/data/web_static/current" ]; then
-    rm -R "/data/web_static/current"
+    rm "/data/web_static/current"
 fi
 
 ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "s/server_name localhost;/server_name _;/" /etc/nginx/sites-available/default
+
 sudo sed -i '17i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
