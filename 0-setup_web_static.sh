cd9d6badde08cfc07c31b68318dfe1fa9b4ebd32
Herman100 <hermankwamebour30@gmail.com>
2023-09-07 00:19:11 +0000

    static setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
-# A script that configures nginx and sets up web static as per specifications
-apt-get -y update
+# creates a deployment script for web static
+
+sudo apt-get -y update
 
 if ! command -v nginx &> /dev/null; then
-	apt-get -y install nginx
+        sudo apt-get -y install nginx
 fi
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
 
-echo "Testing nginx installation" | tee /data/web_static/releases/test/index.html > /dev/null
-
-if [ -L /data/web_static/current/ ]; then
-	rm -R /data/web_static/current/
+if [ -L "/data/web_static/current" ]; then
+    rm -R "/data/web_static/current"
 fi
-ln -sf /data/web_static/releases/test /data/web_static/current
 
-chown -R ubuntu:ubuntu /data/
+ln -s /data/web_static/releases/test/ /data/web_static/current
 
-loc="location /hbnb_static {"
-al="alias /data/web_static/current/;"
+sudo chown -R ubuntu:ubuntu /data/
 
 sed -i "s/server_name localhost;/server_name _;/" /etc/nginx/sites-available/default
-sed -i "s@root /var/www/html;@root /data/web_static/current;@" /etc/nginx/sites-available/default
-sed -i "/server_name _;/a \\\n\t$loc\n\t\t$al\n\t}" /etc/nginx/sites-available/default
-
-service nginx start
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+#sed -i "/server_name _;/a \\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
