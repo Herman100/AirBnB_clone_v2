3f1e3d842e02838c2ca6af511e15a3c028319dac
Herman100 <hermankwamebour30@gmail.com>
2023-09-07 08:05:47 +0000

    code correction

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,13 +21,8 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C \
                 /data/web_static/releases/{}/".format(file_name, name))
         run("rm /tmp/{}".format(file_name))
-        run("rm -rf /data/web_static/releases/{}/images \
-            /data/web_static/releases/{}/styles".format(name, name))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ \
+        run("ln -s /data/web_static/releases/{}/web_static/ \
             /data/web_static/current".format(name))
         return True
     except Exception as e:
