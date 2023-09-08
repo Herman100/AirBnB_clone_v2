911e1be021f93cd4219b72806509b7a7f896e595
Herman100 <hermankwamebour30@gmail.com>
2023-09-08 01:32:49 +0000

    pp for server preparation deploy

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,28 +1,28 @@
-# puppet manifest preparing a server for static content deployment
-exec { 'Update server':
+# puppet file static content deployment
+exec { 'server update':
   command => '/usr/bin/env apt-get -y update',
 }
--> exec {'Install NGINX':
+-> exec {'ensure x':
   command => '/usr/bin/env apt-get -y install nginx',
 }
--> exec {'Creates directory release/test':
+-> exec {'directory release/test':
   command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
 }
--> exec {'Creates directories shared':
+-> exec {'Creates directories':
   command => '/usr/bin/env mkdir -p /data/web_static/shared/',
 }
--> exec {'Write Hello World in index with tee command':
-  command => '/usr/bin/env echo "Hello Wolrd Puppet" | sudo tee /data/web_static/releases/test/index.html',
+-> exec {'custom test':
+  command => '/usr/bin/env echo "simple content" | sudo tee /data/web_static/releases/test/index.html',
 }
--> exec {'Create Symbolic link':
+-> exec {'Sym link':
   command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
 }
--> exec {'Change owner and group like ubuntu':
+-> exec {'ownership and group':
   command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 }
--> exec {'Add new configuration to NGINX':
+-> exec {'configure x':
   command => '/usr/bin/env sed -i "/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
 }
--> exec {'Restart NGINX':
+-> exec {'restart x':
   command => '/usr/bin/env service nginx restart',
 }
