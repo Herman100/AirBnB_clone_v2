b927a66881b473e8f1805ab57d7e2c55398ef6d3
Herman100 <hermankwamebour30@gmail.com>
2023-09-08 01:07:39 +0000

    code pp test

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,15 +7,14 @@ exec { 'Update server':
   command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
 } ~> exec {'Creates directories shared':
   command => '/usr/bin/env mkdir -p /data/web_static/shared/',
-} ~> exec {'Write Hello World in index with tee command':
-  command => '/usr/bin/env echo "Hello Wolrd Puppet" | sudo tee /data/web_static/releases/test/index.html',
+} ~> exec {'Write Simple content in index with tee command':
+  command => '/usr/bin/env echo "Simple content" | sudo tee /data/web_static/releases/test/index.html',
 } ~> exec {'Create Symbolic link':
   command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
 } ~> exec {'Change owner and group like ubuntu':
   command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 } ~> exec {'Add new configuration to NGINX':
-  command => "/usr/bin/env sed -i \"/listen 80 default_server;/a location /hbnb_static/ { 
-alias /data/web_static/current/;}\" /etc/nginx/sites-available/default",
+  command => "/usr/bin/env sed -i \"/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/;}\" /etc/nginx/sites-available/default",
 } ~> exec {'Restart NGINX':
   command => '/usr/bin/env service nginx restart',
 }
