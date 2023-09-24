76b3be06e1d65846345fb872e1cca89eb9731f44
Herman100 <hermankwamebour30@gmail.com>
2023-09-24 20:55:14 +0000

    pycodestyle requirement

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,11 +9,13 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_db(exception=None):
     """function to close the database sessions
     """
     storage.close()
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def list_states():
@@ -22,6 +24,7 @@ def list_states():
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('8-cities_by_states.html', states=states)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
