b2c5510c7ddb6919d43447924c435fc4dda3c152
Herman100 <hermankwamebour30@gmail.com>
2023-09-24 18:57:05 +0000

    rendering models from db

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""A flask python script that returns an html page and
+contains two defined funtions for route and closing db after each session
+"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(exception=None):
+    """function to close the database sessions
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def list_states():
+    """provides a list of all the states to be sorted and rendered
+    """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
