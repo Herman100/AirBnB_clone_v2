b6894a71b07305eda44577520826532533c80070
Herman100 <hermankwamebour30@gmail.com>
2023-09-24 20:50:05 +0000

    web flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,29 +3,25 @@
 contains two defined funtions for route and closing db after each session
 """
 
-from flask import Flask, escape, render_template
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
 
-
 @app.teardown_appcontext
 def close_db(exception=None):
-    """
-    Function to close the current database sessions.
+    """function to close the database sessions
     """
     storage.close()
 
-
 @app.route('/cities_by_states', strict_slashes=False)
-def city_by_states():
+def list_states():
+    """provides a list of all the states to be sorted and rendered
     """
-    Provides a list of all the states to be sorted and rendered.
-    """
-    return render_template('8-cities_by_states.html',
-                           states=storage.all('State').values())
-
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
